50c032d996c84b1492b297cfbda361807e3a7380
Amir Razvi <amirrazvi8@gmail.com>
2025-03-08 15:00:51 +0530

    Add new Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-import { useState, useEffect, useRef } from "react";
+import {useEffect, useRef } from "react";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Experties from "./components/Experties/Experties";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer/Footer";
-import { motion } from "framer-motion";
 import Contact from "./components/Contact/Contact";
 import Skills from "./components/Skill/Skills";
 import { useSelector, useDispatch } from "react-redux";
 import { updateSection } from "./features/scrollComponent/scrollSlice";
-
-
-const getRandom = (min, max) => Math.random() * (max - min) + min;
-function getRandomFromTwo(num1, num2) {
-    const numbers = [num1, num2];
-    const randomIndex = Math.floor(Math.random() * 2);
-    return numbers[randomIndex];
-}
 
 function App() {
     const dispatch = useDispatch();
@@ -30,28 +21,6 @@ function App() {
     };
 
     const darkMode = useSelector((state) => state.theme.darkMode);
-
-    const [squares, setSquares] = useState([]);
-
-    useEffect(() => {
-        const generateSquares = () => {
-            const squaresArray = [];
-            for (let i = 0; i < 30; i++) {
-                squaresArray.push({
-                    size: getRandom(20, 150),
-                    startX: getRandom(-100, window.innerWidth),
-                    startY: getRandom(window.innerHeight - 100, window.innerHeight),
-                    moveX: getRandom(-300, 300),
-                    duration: getRandom(4, 12),
-                    rotate: getRandomFromTwo(0, 25),
-                    round: getRandomFromTwo(20, 100),
-                });
-            }
-            setSquares(squaresArray);
-        };
-
-        generateSquares();
-    }, []);
 
     useEffect(() => {
         const handleIntersection = (entries) => {
@@ -83,32 +52,7 @@ function App() {
   dark:bg-[radial-gradient(ellipse_50%_250%_at_50%_-50%,rgba(120,119,198,0.2),rgba(255,255,255,0))] 
   antialiased selection:bg-cyan-300 selection:text-cyan-950 z-20 bg-gray-200"
                 >
-                    {squares.map((square, index) => (
-                        <motion.div
-                            key={index}
-                            className="absolute bg-blue-500 opacity-50 rotate-45 -z-5"
-                            style={{
-                                width: square.size,
-                                height: square.size,
-                                left: square.startX,
-                                top: square.startY,
-                                rotate: square.rotate,
-                                borderRadius: square.round,
-                            }}
-                            initial={{ y: 100 }}
-                            animate={{
-                                y: -3000,
-                                x: square.moveX,
-                            }}
-                            transition={{
-                                duration: square.duration,
-                                ease: "easeInOut",
-                                repeatType: "loop",
-                            }}
-                        />
-                    ))}
-
-                    <Header/>
+                    <Header />
                     <Home homeRef={sectionRefs.home} />
                     <Experties expertiesRef={sectionRefs.experties} />
                     <Skills skillsRef={sectionRefs.skills} />
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,7 +4,8 @@ import { motion } from "framer-motion";
 import reduxtodo from "../../assets/reduxtodo.png";
 import currencyConv from "../../assets/currencyConv.png";
 import darkPortfolio from "../../assets/darkPortfolio.png";
-import lightPortfolio from "../../assets/lightPortfolio.png"
+import lightPortfolio from "../../assets/lightPortfolio.png";
+import luxora from "../../assets/luxora.png";
 import { useSelector } from "react-redux";
 
 function Projects({ projectsRef }) {
@@ -13,7 +14,7 @@ function Projects({ projectsRef }) {
     const portfolio = darkMode ? lightPortfolio : darkPortfolio;
 
     return (
-        <div ref={projectsRef} id="projects" className={`mt-28 ${darkMode?"dark":""} `}>
+        <div ref={projectsRef} id="projects" className={`mt-28 ${darkMode ? "dark" : ""} `}>
             <div className="text-white text-3xl w-full flex justify-center mb-8">
                 <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1.5 }}>
                     <h2 className="flex text-black dark:text-white md:text-6xl text-3xl items-center mb-8 font-bold ">Projects</h2>
@@ -21,23 +22,39 @@ function Projects({ projectsRef }) {
             </div>
             <div className="flex w-[90%] justify-center ml-[5%] mb-20">
                 <div className="flex flex-wrap justify-center text-white w-full gap-6">
-                    <Card 
-                    link={"https://amirrazvi.vercel.app"}
-                    image={portfolio}
-                    h1={"Personal Portfolio Website"}
-                    h2={'Showcasing My Work with React, Tailwind CSS, and Framer Motion'}
-                    content={'A responsive and dynamic portfolio website designed to highlight my web development projects, skills, and experience. Built using React for fast rendering, React-Redux for efficient state management, and Framer Motion for engaging animations.'}
-                    toph1={'React, Redux-Toolkit,Framer-motion'}
-                    toph2={'TailwindCss,EmailJs'}
+                    <Card
+                        link={"https://luxora-fashionshop.vercel.app"}
+                        image={luxora}
+                        h1={"Luxora - Clothing E-commerce"}
+                        h2={"A Full-Stack MERN E-commerce Platform"}
+                        content={
+                            "Luxora is a feature-rich online clothing store built with the MERN stack. It includes a seamless shopping experience, secure payments, and an admin panel for product and order management."
+                        }
+                        toph1={"MERN Stack, Context API, TailwindCSS"}
+                        toph2={"Framer Motion, Stripe, Nodemailer"}
                     />
-                    <Card 
-                    link={"https://currency-converter-wheat-six.vercel.app"}
-                    image={currencyConv}
-                    h1={"Currency Converter"}
-                    h2={"Effortless Global Exchange"}
-                    content={"Convert currencies in real-time with a user-friendly tool built using React. This application delivers accurate conversion rates across multiple currencies, offering instant results for efficient, on-the-go currency calculations."}
-                    toph1={'React,Api handling'}
-                    toph2={'TailwindCss'}
+
+                    <Card
+                        link={"https://amirrazvi.vercel.app"}
+                        image={portfolio}
+                        h1={"Personal Portfolio Website"}
+                        h2={"Showcasing My Work with React, Tailwind CSS, and Framer Motion"}
+                        content={
+                            "A responsive and dynamic portfolio website designed to highlight my web development projects, skills, and experience. Built using React for fast rendering, React-Redux for efficient state management, and Framer Motion for engaging animations."
+                        }
+                        toph1={"React, Redux-Toolkit,Framer-motion"}
+                        toph2={"TailwindCss,EmailJs"}
+                    />
+                    <Card
+                        link={"https://currency-converter-wheat-six.vercel.app"}
+                        image={currencyConv}
+                        h1={"Currency Converter"}
+                        h2={"Effortless Global Exchange"}
+                        content={
+                            "Convert currencies in real-time with a user-friendly tool built using React. This application delivers accurate conversion rates across multiple currencies, offering instant results for efficient, on-the-go currency calculations."
+                        }
+                        toph1={"React,Api handling"}
+                        toph2={"TailwindCss"}
                     />
                     <Card
                         link={"https://amirreduxtodo.vercel.app"}
